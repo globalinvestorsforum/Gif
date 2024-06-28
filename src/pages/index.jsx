4251896aa3ad9d67217632b3d184f5a5f6b8629e
unstoppableayush <ayushkum884@gmail.com>
2024-06-28 15:41:16 +0530

    Terms and condition and privacy policy added

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,3 +3,5 @@ export { default as WhatWeProduce } from './WhatWeProduce/WhatWeProduce.jsx';
 export { default as Membership } from './Membership/Membership.jsx';
 export { default as WhoWeAre } from './WhoWeAre/WhoWeAre.jsx';
 export { default as Contact } from './Contact/Contact.jsx';
+export { default as Terms } from './Terms/Terms.jsx';
+export { default as Privacy } from './Privacy/Privacy.jsx';
