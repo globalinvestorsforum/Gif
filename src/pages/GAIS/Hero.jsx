6d6ab0cd6ebbd8861654a179632f95c0bec1ba5d
rahul <rahulmudgal53@gmail.com>
2025-02-12 08:34:53 +0530

    updated the gais homepage event Dates

--- src/pages/GAIS/Hero.jsx
+++ src/pages/GAIS/Hero.jsx
@@ -20,7 +20,7 @@ function Hero() {
               LATEST ADVANCEMENTS AND TRENDS OF AI.
             </h2>
             <p className="pt-6 px-3  text-lg md:text-1xl lg:text-2xl font-semibold  text-neutral-200  animate-slidein [--slidein-delay:400ms]  opacity-1 ">
-              2nd & 3rd May, 2025
+              1st & 2nd July, 2025
             </p>
             <p className="pt-4 px-3 text-lg md:text-1xl lg:text-2xl font-semibold    text-neutral-200 animate-slidein [--slidein-delay:500ms]  opacity-1  ">
               Berlin, Germany.
