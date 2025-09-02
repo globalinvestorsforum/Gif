fbc148da34a1dc45bb6158e92c64e1cca932bed6
Sajag Agrawal <sajagaga2806@gmail.com>
2025-09-02 12:33:22 +0530

    Logos Fixed

--- src/constants/AttendeeData.js
+++ src/constants/AttendeeData.js
@@ -115,10 +115,10 @@ import Omdena from '../assets/AllAttendees/Company-logo/Omdena.jpeg';
 import Bragi from '../assets/AllAttendees/Company-logo/Bragi.jpeg';
 import vGreens from '../assets/AllAttendees/Company-logo/vGreens.png';
 import Arboair from '../assets/AllAttendees/Company-logo/Arboair.png';
-import Luxurynsight from '../assets/AllAttendees/Company-logo/Luxurynsight.jpg';
+import Luxurynsight from '../assets/AllAttendees/Company-logo/Luxurynsight.svg';
 import optocycle from '../assets/AllAttendees/Company-logo/optocycle.png';
 import ScavengerAi from '../assets/AllAttendees/Company-logo/ScavengerAI.jpg';
-import TextCortex from '../assets/AllAttendees/Company-logo/TextCortext.jpg';
+import TextCortex from '../assets/AllAttendees/Company-logo/TextCortex.svg';
 import Visium from '../assets/AllAttendees/Company-logo/Visium.png';
 
 
