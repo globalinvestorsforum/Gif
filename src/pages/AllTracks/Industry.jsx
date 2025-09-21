b9c4e91e2dc34025f19d7d38b2cade1c20aa8ddf
rahul <rahulmudgal53@gmail.com>
2025-09-21 17:21:29 +0530

    Added Wrapper div for white background and full width to Industry page

--- src/pages/AllTracks/Industry.jsx
+++ src/pages/AllTracks/Industry.jsx
@@ -1,42 +1,43 @@
-import * as react from 'react';
-import Retail from './IndustryTracks/Retail.jsx';
+import * as react from "react";
+import Retail from "./IndustryTracks/Retail.jsx";
 // import Finance from './IndustryTracks/Finance.jsx';
-import Education from './IndustryTracks/Education.jsx';
-import Autotech from './IndustryTracks/Autotech.jsx';
-import Agriculture from './IndustryTracks/Agriculture.jsx';
-import Maritime from './IndustryTracks/Maritime.jsx';
-import Robotics from './IndustryTracks/Robotics.jsx';
-import Transportation from './IndustryTracks/Transportation.jsx';
-import Manufacturing from './IndustryTracks/Manufacturing.jsx';
-import Energy from './IndustryTracks/Energy.jsx';
-import Cybersecurity from './IndustryTracks/Cybersecurity.jsx';
-import Massmedia from './IndustryTracks/Massmedia.jsx';
-import Autonomous from './IndustryTracks/Autonomous.jsx';
-import Gaming from './IndustryTracks/Gaming.jsx';
-import HealthCare1 from './IndustryTracks/HealthCare1.jsx'
-import HealthCare2 from './IndustryTracks/HealthCare2.jsx'
-import HealthCare3 from './IndustryTracks/HealthCare3.jsx'
-export default function Industry(){
-    return(
-        <>
-        <div classname="bg-white min-h-screen">
-        <Retail/>
+import Education from "./IndustryTracks/Education.jsx";
+import Autotech from "./IndustryTracks/Autotech.jsx";
+import Agriculture from "./IndustryTracks/Agriculture.jsx";
+import Maritime from "./IndustryTracks/Maritime.jsx";
+import Robotics from "./IndustryTracks/Robotics.jsx";
+import Transportation from "./IndustryTracks/Transportation.jsx";
+import Manufacturing from "./IndustryTracks/Manufacturing.jsx";
+import Energy from "./IndustryTracks/Energy.jsx";
+import Cybersecurity from "./IndustryTracks/Cybersecurity.jsx";
+import Massmedia from "./IndustryTracks/Massmedia.jsx";
+import Autonomous from "./IndustryTracks/Autonomous.jsx";
+import Gaming from "./IndustryTracks/Gaming.jsx";
+import HealthCare1 from "./IndustryTracks/HealthCare1.jsx";
+import HealthCare2 from "./IndustryTracks/HealthCare2.jsx";
+import HealthCare3 from "./IndustryTracks/HealthCare3.jsx";
+export default function Industry() {
+  return (
+    <>
+    <div className="bg-white w-full">
+      <div classname=" min-h-screen">
+        <Retail />
         {/* <Finance/> */}
-        <Education/>
-        <Autotech/>
-        <Maritime/>
-        <Robotics/>
-        <Transportation/>
-        <Manufacturing/>
-        <Energy/>
-        <Cybersecurity/>
-        <Massmedia/>
-        <Autonomous/>
-        <Gaming/>
-        <HealthCare1/>
-        <HealthCare2/>
-        <HealthCare3/>
-        <Agriculture/>
+        <Education />
+        <Autotech />
+        <Maritime />
+        <Robotics />
+        <Transportation />
+        <Manufacturing />
+        <Energy />
+        <Cybersecurity />
+        <Massmedia />
+        <Autonomous />
+        <Gaming />
+        <HealthCare1 />
+        <HealthCare2 />
+        <HealthCare3 />
+        <Agriculture />
         {/* <HealthCare1/>
         <HealthCare2/>
         <HealthCare3/>
@@ -51,8 +52,8 @@ export default function Industry(){
         <Autonomous/>
         <Gaming/>
         <Agriculture/> */}
-        
-        </div>
-        </>
-    )
+      </div>
+      </div>
+    </>
+  );
 }
