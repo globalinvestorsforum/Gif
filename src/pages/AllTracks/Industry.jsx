bcabbfb636bdc73e711ef874117802b486141847
Sajag Agrawal <sajagaga2806@gmail.com>
2025-09-21 11:48:10 +0530

    ch5

--- src/pages/AllTracks/Industry.jsx
+++ src/pages/AllTracks/Industry.jsx
@@ -1,27 +1,27 @@
 import * as react from 'react';
 import Retail from './IndustryTracks/Retail.jsx';
-// import Finance from './IndustryTracks/Finance.jsx';
-// import Education from './IndustryTracks/Education.jsx';
-// import Autotech from './IndustryTracks/Autotech.jsx';
-// import Agriculture from './IndustryTracks/Agriculture.jsx';
-// import Maritime from './IndustryTracks/Maritime.jsx';
-// import Robotics from './IndustryTracks/Robotics.jsx';
-// import Transportation from './IndustryTracks/Transportation.jsx';
-// import Manufacturing from './IndustryTracks/Manufacturing.jsx';
-// import Energy from './IndustryTracks/Energy.jsx';
-// import Cybersecurity from './IndustryTracks/Cybersecurity.jsx';
-// import Massmedia from './IndustryTracks/Massmedia.jsx';
-// import Autonomous from './IndustryTracks/Autonomous.jsx';
-// import Gaming from './IndustryTracks/Gaming.jsx';
-// import HealthCare1 from './IndustryTracks/HealthCare1.jsx'
-// import HealthCare2 from './IndustryTracks/HealthCare2.jsx'
-// import HealthCare3 from './IndustryTracks/HealthCare3.jsx'
+import Finance from './IndustryTracks/Finance.jsx';
+import Education from './IndustryTracks/Education.jsx';
+import Autotech from './IndustryTracks/Autotech.jsx';
+import Agriculture from './IndustryTracks/Agriculture.jsx';
+import Maritime from './IndustryTracks/Maritime.jsx';
+import Robotics from './IndustryTracks/Robotics.jsx';
+import Transportation from './IndustryTracks/Transportation.jsx';
+import Manufacturing from './IndustryTracks/Manufacturing.jsx';
+import Energy from './IndustryTracks/Energy.jsx';
+import Cybersecurity from './IndustryTracks/Cybersecurity.jsx';
+import Massmedia from './IndustryTracks/Massmedia.jsx';
+import Autonomous from './IndustryTracks/Autonomous.jsx';
+import Gaming from './IndustryTracks/Gaming.jsx';
+import HealthCare1 from './IndustryTracks/HealthCare1.jsx'
+import HealthCare2 from './IndustryTracks/HealthCare2.jsx'
+import HealthCare3 from './IndustryTracks/HealthCare3.jsx'
 export default function Industry(){
     return(
         <>
         <Retail/>
         <h1>Hello Industry</h1>
-        {/* <Finance/>
+        <Finance/>
         <Education/>
         <HealthCare1/>
         <HealthCare2/>
@@ -36,8 +36,8 @@ export default function Industry(){
         <Massmedia/>
         <Autonomous/>
         <Gaming/>
-        <Agriculture/> */}
-        
+        <Agriculture/>
+        <h1>Hello Industry</h1>
         
         </>
     )
