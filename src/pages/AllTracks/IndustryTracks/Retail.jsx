732e517ed173e33d028c1b848d8afcdf079c2aa3
Sajag Agrawal <sajagaga2806@gmail.com>
2025-09-23 13:03:19 +0530

    added links for test

--- src/pages/AllTracks/IndustryTracks/Retail.jsx
+++ src/pages/AllTracks/IndustryTracks/Retail.jsx
@@ -349,7 +349,7 @@ const AgendaPage = () => {
   <button className="bg-black text-white px-8 py-3 rounded-md font-semibold text-base hover:bg-gray-800 transition-all duration-300 hover:scale-105 hover:shadow-xl transform" onClick={()=>{navigate("/gais/conference")}}>
     View All Tracks
   </button>
-  <button className="bg-black text-white px-8 py-3 rounded-md font-semibold text-base hover:bg-gray-800 transition-all duration-300 hover:scale-105 hover:shadow-xl transform">
+  <button className="bg-black text-white px-8 py-3 rounded-md font-semibold text-base hover:bg-gray-800 transition-all duration-300 hover:scale-105 hover:shadow-xl transform" onClick={()=>{navigate("/gais/conference/ai-Powered-Retail-E-commerce-Conclave-summit")}}>
     More About Tracks
   </button>
 </div>
